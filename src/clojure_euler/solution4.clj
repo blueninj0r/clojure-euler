1bfe23806d236c38070a65fed5b9ca3591929637
Christopher McCluskey <christopher.i.mccluskey@gmail.com>
2013-03-31 11:48:03 +0100

    Finished problem 4.

--- src/clojure_euler/solution4.clj
+++ src/clojure_euler/solution4.clj
@@ -1,9 +1,18 @@
 (require '[clojure.string :as s])
 
-(defn solution-4
-	[n]
-	(println n))
-
 (defn is-palindrome?
 	[n]
 	(= (.toString n) (s/reverse (.toString n))))
+
+(defn generate-products
+	[n]
+	(let [nums (range 100 1000)]
+		(for [x nums
+			  y nums
+			  :when (is-palindrome? (* x y))]
+			  (* x y))))
+
+(defn solution-4
+	[n]
+	(reduce max (generate-products n)))
+
